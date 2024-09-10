bbb7e6f619cd5d58f91b13c83b21d344f0392afd
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2024-09-10 15:05:09 -0400

    Create SirumatekApplicationTests.java

---
+++ Backend/src/test/java/com/example/sirumatek/SirumatekApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.sirumatek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SirumatekApplicationTests {
+    @Test
+    public void contextLoads() {
+        // Esta prueba asegura que el contexto de la aplicación Spring se cargue
+        // correctamente.
+    }
+
+}
