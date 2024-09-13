f35085634a1c947034487783295a5df556ef8bb9
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2024-09-13 07:57:51 -0400

    Add files via upload

---
+++ Frontend (test)/src/app/models/empleado.model.ts
@@ -0,0 +1,11 @@
+export interface Empleado {
+  id?: number;
+  cedula: string;
+  nombre: string;
+  apellido: string;
+  fechaNacimiento: string;
+  fechaIngreso: string;
+  sexo: string;
+  correo: string;
+  telefono: string;
+}
