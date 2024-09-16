d5ee02fa780f49295aedcb3a1e5994aec8c70a70
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2024-09-16 11:22:09 -0400

    Add files via upload

--- Frontend (test)/src/app/login/login.component.ts
+++ Frontend (test)/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +11,19 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private router: Router, private AuthService: AuthService) {}
 
   login() {
-    this.http.post('http://localhost:8080/api/auth/login', { email: this.email, password: this.password },
-      { headers: { 'Content-Type': 'application/json' } }
+
+    this.AuthService.login(this.email, this.password).subscribe(
+      (response) => {
+        console.log(response)
+        this.router.navigate(['/menu-admin']);
+      },
+      (error) => {
+        console.log(error)
+        alert('Credenciales incorrectas');
+      }
     )
-      .subscribe(
-        response => {
-          alert('Login exitoso');
-          this.router.navigate(['/menu-admin']);
-        },
-        error => {
-          console.log(error)
-          alert('Credenciales incorrectas');
-        }
-      );
   }
 }
