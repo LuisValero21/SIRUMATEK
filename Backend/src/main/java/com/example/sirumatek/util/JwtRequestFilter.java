e5ff0ec423605a3a3a5503f4016cebb1003d74b9
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2024-09-25 09:53:37 -0400

    Add files via upload

---
+++ Backend/src/main/java/com/example/sirumatek/util/JwtRequestFilter.java
@@ -0,0 +1,48 @@
+package com.example.sirumatek.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private FilterChain chain;
+
+    public void doFilterInternal()
+            throws ServletException, IOException {
+        doFilterInternal((HttpServletRequest) null, (HttpServletResponse) null, (FilterChain) null);
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        this.request = request;
+        this.response = response;
+        this.chain = chain;
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Aquí puedes cargar el usuario y setear la autenticación en el contexto
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
---
+++ Backend/src/main/java/com/example/sirumatek/util/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.example.sirumatek.util;
+
+import com.example.sirumatek.config.MyConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private MyConfig secretKey = new MyConfig(); // Cambia esto en producción
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // Expira en 10 horas
+                .signWith(SignatureAlgorithm.HS256, secretKey.getSecret_key())
+                .compact();
+    }
+
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey.getSecret_key())
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey.getSecret_key())
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+}
