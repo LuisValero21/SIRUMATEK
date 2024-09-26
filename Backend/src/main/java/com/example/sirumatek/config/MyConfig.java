088c2b7896091f03854073dd2d11f1f1600a914d
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2024-09-26 16:49:15 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/config/MyConfig.java
+++ Backend/src/main/java/com/example/sirumatek/config/MyConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "mi")
+@ConfigurationProperties(prefix = "jwt")
 public class MyConfig {
 
     private String secret_key;
@@ -12,14 +12,20 @@ public class MyConfig {
 
     // Getters y setters
     public String getSecret_key() {
+        if (secret_key == null || secret_key.isEmpty()) {
+            throw new IllegalStateException("Secret key is not set or is empty");
+        }
         return secret_key;
     }
 
-    public void setSecret_key(String variable) {
-        this.secret_key = variable;
+    public void setSecret_key(String secret_key) {
+        this.secret_key = secret_key;
     }
 
     public String getFront_url() {
+        if (front_url == null || front_url.isEmpty()) {
+            throw new IllegalStateException("Front URL is not set or is empty");
+        }
         return front_url;
     }
 
