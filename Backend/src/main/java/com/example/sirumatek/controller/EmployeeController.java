c3df83ddf2edee67d95c58af7f96747262fed09b
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-01-29 14:55:25 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/controller/EmployeeController.java
+++ Backend/src/main/java/com/example/sirumatek/controller/EmployeeController.java
@@ -5,18 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.sirumatek.model.Employee;
 import com.example.sirumatek.service.EmployeeService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/empleados")
 public class EmployeeController {
