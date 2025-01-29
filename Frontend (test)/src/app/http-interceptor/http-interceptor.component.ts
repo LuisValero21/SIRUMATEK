88ef39f9271e6e413124f1e3a20181e12496e023
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-01-29 15:01:31 -0400

    Add files via upload

--- Frontend (test)/src/app/http-interceptor/http-interceptor.component.ts
+++ Frontend (test)/src/app/http-interceptor/http-interceptor.component.ts
@@ -1,28 +1,25 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Component, Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-@Component({
-  selector: 'app-http-interceptor',
-  standalone: true,
-  imports: [],
-  templateUrl: './http-interceptor.component.html',
-  styleUrl: './http-interceptor.component.css'
-})
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('authToken');
-    
+
+    console.log('Token obtenido:', token); // Depuración
+
     if (token) {
       const cloned = req.clone({
-        headers: req.headers.set("Authorization", "Bearer " + token)
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
+
       return next.handle(cloned);
     }
-    
+
     return next.handle(req);
   }
 }
