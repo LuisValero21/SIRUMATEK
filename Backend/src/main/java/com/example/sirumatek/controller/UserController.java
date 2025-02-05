28962bd1be0c6f5bd0c0edd2a71ce7a59b8cc418
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-02-05 09:07:30 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/controller/UserController.java
+++ Backend/src/main/java/com/example/sirumatek/controller/UserController.java
@@ -4,6 +4,11 @@ import com.example.sirumatek.model.User;
 import com.example.sirumatek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +19,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public List<User> getAllUsers() {
