6b18d98f04416cdc87e43a239833551197cf0a20
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-02-05 09:06:25 -0400

    Create JwtFilter.java

---
+++ Backend/src/main/java/com/example/sirumatek/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.example.sirumatek.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Collections;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final String SECRET_KEY = Base64.getEncoder()
+            .encodeToString("c1b27bca-bf43-4da2-8c2b-6966d556d361".getBytes());
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.replace("Bearer ", "");
+
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String username = claims.getSubject();
+
+            if (username != null) {
+                UserDetails userDetails = new User(username, "", Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(new JwtAuthentication(userDetails));
+            }
+
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
