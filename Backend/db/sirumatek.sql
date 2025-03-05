7544adddf67ae03d271d3420fde6e72c4f76f553
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-05 11:49:02 -0400

    Add files via upload

--- Backend/db/sirumatek.sql
+++ Backend/db/sirumatek.sql
@@ -7,7 +7,7 @@ CREATE TABLE usuario (
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(100) NOT NULL UNIQUE,
     contrasena VARCHAR(255) NOT NULL,
-    rol VARCHAR(10) NOT NULL
+    rol VARCHAR(100) NOT NULL
 );
 
 -- Crear tabla Empleado
@@ -27,8 +27,7 @@ CREATE TABLE empleado (
 -- Crear tabla Departamento
 CREATE TABLE departamento (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL UNIQUE,
-    CONSTRAINT chk_departamento CHECK (nombre = 'RRHH')
+    nombre VARCHAR(100) NOT NULL
 );
 
 -- Crear tabla Empleado_Departamento para la relación entre Empleado y Departamento
@@ -38,4 +37,25 @@ CREATE TABLE empleado_departamento (
     PRIMARY KEY (empleado_id, departamento_id),
     FOREIGN KEY (empleado_id) REFERENCES empleado(id) ON DELETE CASCADE,
     FOREIGN KEY (departamento_id) REFERENCES departamento(id) ON DELETE CASCADE
+);
+
+-- Crear tabla Tipo_Notificacion
+CREATE TABLE tipo_notificacion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(255) NOT NULL
+);
+
+-- Crear tabla Notificacion_Correo
+CREATE TABLE notificacion_correo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_tipo_notificacion INT NOT NULL,
+    id_usuario INT NOT NULL,
+    id_mensaje VARCHAR(255) NOT NULL,
+    id_correo_masivo INT NULL,
+    destinatario VARCHAR(255) NOT NULL,
+    estatus VARCHAR(10) NOT NULL,
+    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT chk_estatus CHECK (estatus IN ('ENVIADO', 'ERROR', 'PENDIENTE')),
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tipo_notificacion FOREIGN KEY (id_tipo_notificacion) REFERENCES tipo_notificacion(id) ON DELETE CASCADE
 );
