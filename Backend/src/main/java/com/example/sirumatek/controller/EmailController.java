6c55a91ca15964906674480119ce2a38bcc19e80
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-11 09:57:17 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/controller/EmailController.java
+++ Backend/src/main/java/com/example/sirumatek/controller/EmailController.java
@@ -2,9 +2,9 @@ package com.example.sirumatek.controller;
 
 import com.example.sirumatek.service.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,22 +15,14 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/enviar")
-    public ResponseEntity<String> enviarCorreo(@RequestBody Map<String, Object> request) {
-        try {
-            String destinatario = (String) request.get("destinatario");
-            String asunto = (String) request.get("asunto");
-            String templateId = (String) request.get("templateId");
-            Map<String, String> variables = (Map<String, String>) request.get("variables");
+    @PostMapping("/enviar-cumpleanos")
+    public String enviarCorreoCumpleanos(@RequestParam String destinatario, @RequestParam String nombreDestinatario) {
+        String asunto = "¡Feliz cumpleaños!";
+        String templateId = "d-96fcfb13970f4d81b7bd8980786501bc";
 
-            if (destinatario == null || asunto == null || templateId == null) {
-                return ResponseEntity.badRequest().body("Faltan campos obligatorios");
-            }
+        Map<String, String> variables = new HashMap<>();
+        variables.put("nombre", nombreDestinatario);
 
-            String resultado = emailService.enviarCorreo(destinatario, asunto, templateId, variables);
-            return ResponseEntity.ok(resultado);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error en el servidor: " + e.getMessage());
-        }
+        return emailService.enviarCorreo(destinatario, nombreDestinatario, asunto, templateId, variables);
     }
 }
