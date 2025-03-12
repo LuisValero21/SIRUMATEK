fe8f7bd3791197e79fdc8c2cea77882dd7c8a674
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-12 10:17:26 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/repository/EmployeeRepository.java
+++ Backend/src/main/java/com/example/sirumatek/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.example.sirumatek.repository;
 import com.example.sirumatek.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "SELECT * FROM empleado", nativeQuery = true)
     List<Employee> findAllEmployees();
     void deleteById(Long id);
+
+    @Query("SELECT e FROM Employee e WHERE MONTH(e.fechaNacimiento) = :mes AND DAY(e.fechaNacimiento) = :dia")
+    List<Employee> findByMonthAndDay(@Param("mes") int mes, @Param("dia") int dia);
 }
