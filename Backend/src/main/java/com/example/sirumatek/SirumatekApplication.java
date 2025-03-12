1ceb11e7b482dfd9c0d3ff05bc2428a6509b3d34
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-12 08:49:36 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/SirumatekApplication.java
+++ Backend/src/main/java/com/example/sirumatek/SirumatekApplication.java
@@ -2,8 +2,12 @@ package com.example.sirumatek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.example.sirumatek")
+@Configuration
+@EnableScheduling
 public class SirumatekApplication {
     public static void main(String[] args) {
         SpringApplication.run(SirumatekApplication.class, args);
