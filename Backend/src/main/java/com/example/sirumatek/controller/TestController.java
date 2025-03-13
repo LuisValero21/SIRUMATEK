104f1f14f87987e7832300bae87e5e5f389314a3
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-13 09:47:03 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/controller/TestController.java
+++ Backend/src/main/java/com/example/sirumatek/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.sirumatek.controller;
 
 import com.example.sirumatek.model.Employee;
 import com.example.sirumatek.repository.EmployeeRepository;
+import com.example.sirumatek.service.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class TestController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    private EmailService emailService;
 
     @GetMapping
     public ResponseEntity<String> test() {
@@ -27,5 +29,11 @@ public class TestController {
     public List<Employee> obtenerCumpleanerosHoy() {
         LocalDate today = LocalDate.now();
         return employeeRepository.findByMonthAndDay(today.getMonthValue(), today.getDayOfMonth());
+    }
+
+    @GetMapping("/enviar-cumpleanos")
+    public String testEnvioCorreos() {
+        emailService.enviarCorreosCumpleanos();
+        return "Proceso de verificación ejecutado.";
     }
 }
