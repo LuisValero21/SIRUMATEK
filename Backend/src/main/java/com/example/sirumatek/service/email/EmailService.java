f86015aa90b00af9f092826e6920459fb02b4b2b
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-03-13 09:43:19 -0400

    Add files via upload

--- Backend/src/main/java/com/example/sirumatek/service/email/EmailService.java
+++ Backend/src/main/java/com/example/sirumatek/service/email/EmailService.java
@@ -69,7 +69,7 @@ public class EmailService {
         }
     }
 
-    @Scheduled(cron = "0 0 9 * * ?")
+    @Scheduled(cron = "0 0 10 * * ?")
     public void enviarCorreosCumpleanos() {
         LocalDate today = LocalDate.now();
         int mesActual = today.getMonthValue();
